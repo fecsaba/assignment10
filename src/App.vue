de4e5565102238210c49daa9ac2eb7ab0e82f03a
fecsaba <nexusegyesulet@gmail.com>
2020-01-13 12:58:29 +0100

    Working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1
+          :class="color"
+          v-myOn:mouseover="mouseEnter"
+          v-myOn:mouseleave="mouseLeave">Directives Exercise</h1>
+        <!-- Exercise -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
+  },
+  directives: {
+    'myOn': {
+      bind(el, binding) {
+        let type = binding.arg
+        let fn = binding.value
+
+        el.addEventListener(type, fn)
+      }
+    }
+  },
+  methods: {
+    mouseEnter() {
+      this.color = 'red'
+      // eslint-disable-next-line no-console
+      console.log('Mouse entered!');
+    },
+    mouseLeave() {
+      this.color = 'blue'
+      // eslint-disable-next-line no-console
+      console.log('Mouse leaved!');
+    }
+  },
+  data: function () {
+    return {color: ''}
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .red {
+    color: red;
+  }
+  .blue {
+    color: blue;
+  }
 </style>
